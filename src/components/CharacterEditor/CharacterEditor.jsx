e0e5ddfaf65689c978e603d9e25700d774c30596
Khanh Tran <khanhtranngoccva@gmail.com>
2025-11-23 16:54:30 +0700

    initial: initial commit

--- src/components/CharacterEditor/CharacterEditor.jsx
+++ src/components/CharacterEditor/CharacterEditor.jsx
@@ -29,8 +29,9 @@ function App() {
   );
 
   return (
-    <main className={styles.characterEditor}>
-      <MaxWidthWrapper className={styles.maxWidthWrapper}>
+    <MaxWidthWrapper>
+      <main className={styles.characterEditor}>
+        <div className={styles.characterWrapperBackground}></div>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
@@ -38,57 +39,62 @@ function App() {
             controls below. What sort of adventure will you embark on?{' '}
           </p>
         </header>
-        <div className={styles.controlColumn}>
-          <ControlPane
-            title="Bodies"
-            options={bodyOptions}
-            currentOption={body}
-            handleSelectOption={setBody}
-          />
-          <ControlPane
-            title="Heads"
-            options={headOptions}
-            currentOption={head}
-            handleSelectOption={setHead}
-          />
-          <ControlPane
-            title="Faces"
-            options={faceOptions}
-            currentOption={face}
-            handleSelectOption={setFace}
-          />
-          <ControlPane
-            title="Accessories"
-            options={accessoryOptions}
-            currentOption={accessory}
-            handleSelectOption={setAccessory}
-          />
-          <ControlPane
-            title="Skin Color"
-            options={skinColorOptions}
-            currentOption={skinColor}
-            handleSelectOption={setSkinColor}
-          />
-          <ControlPane
-            title="Clothing Color"
-            options={clothesColorOptions}
-            currentOption={clothesColor}
-            handleSelectOption={setClothesColor}
-          />
+        <div className={styles.characterEditorContent}>
+          <div className={styles.controlColumnScroll}>
+            <div className={styles.controlColumn}>
+              <ControlPane
+                title="Bodies"
+                options={bodyOptions}
+                currentOption={body}
+                handleSelectOption={setBody}
+              />
+              <ControlPane
+                title="Heads"
+                options={headOptions}
+                currentOption={head}
+                handleSelectOption={setHead}
+              />
+              <ControlPane
+                title="Faces"
+                options={faceOptions}
+                currentOption={face}
+                handleSelectOption={setFace}
+              />
+              <ControlPane
+                title="Accessories"
+                options={accessoryOptions}
+                currentOption={accessory}
+                handleSelectOption={setAccessory}
+              />
+              <ControlPane
+                title="Skin Color"
+                options={skinColorOptions}
+                currentOption={skinColor}
+                handleSelectOption={setSkinColor}
+              />
+              <ControlPane
+                title="Clothing Color"
+                options={clothesColorOptions}
+                currentOption={clothesColor}
+                handleSelectOption={setClothesColor}
+              />
+            </div>
+          </div>
+          <div className={styles.characterWrapper}>
+            <div className={styles.characterWrapperInner}>
+              <Character
+                body={body}
+                head={head}
+                face={face}
+                accessory={accessory}
+                skinColor={skinColor}
+                clothesColor={clothesColor}
+              />
+            </div>
+          </div>
         </div>
-      </MaxWidthWrapper>
-
-      <div className={styles.characterWrapper}>
-        <Character
-          body={body}
-          head={head}
-          face={face}
-          accessory={accessory}
-          skinColor={skinColor}
-          clothesColor={clothesColor}
-        />
-      </div>
-    </main>
+      </main>
+    </MaxWidthWrapper>
   );
 }
 
